afdc654f0fcae4a37e2598227a58a5eeadc422a3
hollie-rawlings <hollie.rawlings@pharmagenesis.com>
2021-05-26 14:59:44 +0100

    Getting first shiny app working in the deforestation markdown

---
+++ as_sunburst.R
@@ -0,0 +1,43 @@
+as.sunburstDF <- function(DF, valueCol = NULL){
+  require(data.table)
+  
+  colNamesDF <- names(DF)
+  
+  if(is.data.table(DF)){
+    DT <- copy(DF)
+  } else {
+    DT <- data.table(DF, stringsAsFactors = FALSE)
+  }
+  
+  DT[, root := "Total"]
+  colNamesDT <- names(DT)
+  
+  if(is.null(valueCol)){
+    setcolorder(DT, c("root", colNamesDF))
+  } else {
+    setnames(DT, valueCol, "values", skip_absent=TRUE)
+    setcolorder(DT, c("root", setdiff(colNamesDF, valueCol), "values"))
+  }
+  
+  hierarchyCols <- setdiff(colNamesDT, "values")
+  hierarchyList <- list()
+  
+  for(i in seq_along(hierarchyCols)){
+    currentCols <- colNamesDT[1:i]
+    if(is.null(valueCol)){
+      currentDT <- unique(DT[, ..currentCols][, values := .N, by = currentCols], by = currentCols)
+    } else {
+      currentDT <- DT[, lapply(.SD, sum, na.rm = TRUE), by=currentCols, .SDcols = "values"]
+    }
+    setnames(currentDT, length(currentCols), "labels")
+    hierarchyList[[i]] <- currentDT
+  }
+  
+  hierarchyDT <- rbindlist(hierarchyList, use.names = TRUE, fill = TRUE)
+  
+  parentCols <- setdiff(names(hierarchyDT), c("labels", "values", valueCol))
+  hierarchyDT[, parents := apply(.SD, 1, function(x){fifelse(all(is.na(x)), yes = NA_character_, no = paste(x[!is.na(x)], sep = ":", collapse = " - "))}), .SDcols = parentCols]
+  hierarchyDT[, ids := apply(.SD, 1, function(x){paste(x[!is.na(x)], collapse = " - ")}), .SDcols = c("parents", "labels")]
+  hierarchyDT[, c(parentCols) := NULL]
+  return(hierarchyDT)
+}
